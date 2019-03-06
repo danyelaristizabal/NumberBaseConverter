6ad827aaf9d667b85656cac36f97708f6e5aed11
Dani Danyel <danyelaristizabal@hotmail.com>
2019-03-06 14:01:52 +0300

    Firs  Commit

---
+++ NumericBaseConverter/NumericBaseConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumericBaseConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
